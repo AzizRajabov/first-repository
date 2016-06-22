87b40bd6ff3b838986a383972944943974cd2351
Aziz Rajabov <rajabov96.a.a@gmail.com>
2016-06-22 22:47:11 +0500

    third commit

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 </head>
 <body>
     <h1>Hello Git</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, iste placeat quibusdam rerum ullam voluptatem! Aperiam consectetur ea et facilis ipsam labore molestias, nemo possimus quia repudiandae tempora veritatis, vero?</p>
 </body>
 </html>
